4ff7334abf9a9e823a9986e8551b79c040de5ec4
Sovan Roy <roysovan00@gmail.com>
2024-04-04 18:45:39 +0530

    initial commit

---
+++ server/config/connection.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+
+    console.log(`\nMONGODB connected`);
+  } catch (error) {
+    console.log("MONGODB connection error", error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
