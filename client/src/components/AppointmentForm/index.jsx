de78485f4dbfe901e18af5b26b662faf41a7d976
rupal-draft <rupalpaulgspr@gmail.com>
2024-05-02 02:39:31 +0530

    Check for the Doctor_name

--- client/src/components/AppointmentForm/index.jsx
+++ client/src/components/AppointmentForm/index.jsx
@@ -3,51 +3,142 @@ import axios from "axios";
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import Datetime from "react-datetime";
 import "react-datetime/css/react-datetime.css";
 import { toast } from "react-toastify";
 import { departments } from "../data";
+import { useEffect } from "react";
 
 export default function AppointmentForm() {
-  const [selectedDate, setSelectedDate] = useState(null);
-  const [dob, setSelectedDateofBirth] = useState(null);
-  const [firstname, setFirstName] = useState("");
-  const [lastname, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [phone, setPhone] = useState(0);
-  const [address, setAddress] = useState("");
-  const [selectedDepartment, setSelectedDepartment] = useState("");
-  const [gender, setGender] = useState("");
-  const [doctor_name, setDoctor] = useState("");
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    dob: "",
+    gender: "",
+    appointmentDateTime: new Date(),
+    department: "",
+    doctor_name: "",
+    address: "",
+  });
 
-  const handleGenderChange = (event) => {
-    setGender(event.target.value);
+  const [doctors, setDoctors] = useState([]);
+  const [selectedDoctor, setSelectedDoctor] = useState(null);
+
+  useEffect(() => {
+    if (formData.department) {
+      fetchDoctors(formData.department);
+    }
+  }, [formData.department]);
+
+  const fetchDoctors = async (department) => {
+    try {
+      const response = await axios.get(
+        `http://localhost:4000/api/v1/user/doctor/${createSlug(department)}`,
+        {
+          withCredentials: true,
+        }
+      );
+      setDoctors(response.data.doctors);
+    } catch (error) {
+      console.error("Failed to fetch doctors:", error);
+      toast.error("Failed to fetch doctors");
+    }
+  };
+
+  useEffect(() => {
+    if (formData.doctor_name) {
+      const doctor = doctors.find((doc) => doc.name === formData.doctor_name);
+      setSelectedDoctor(doctor);
+    }
+  }, [formData.doctor_name, doctors]);
+
+  function createSlug(text) {
+    return text
+      .toLowerCase() // Convert text to lowercase
+      .replace(/\s+/g, "-") // Replace spaces with hyphens
+      .replace(/[^\w\-]+/g, "") // Remove all non-word characters
+      .replace(/\-\-+/g, "-"); // Replace multiple hyphens with a single hyphen
+  }
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    console.log(name);
+    if (name === "doctor_name") {
+      const selectedDoctorName =
+        event.target.options[event.target.selectedIndex].text;
+      setFormData((prevState) => ({
+        ...prevState,
+        [name]: selectedDoctorName,
+      }));
+    } else {
+      setFormData((prevState) => ({
+        ...prevState,
+        [name]: value,
+      }));
+    }
+  };
+
+  const handleDateChange = (date) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      appointmentDateTime: date,
+    }));
   };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    const formData = {
-      firstname,
-      lastname,
-      email,
-      phone,
-      dob: dob ? dob.toISOString() : null,
-
-      gender,
-      appointmentDateTime: selectedDate.toISOString(),
-      selectedDepartment,
-      doctor_name,
-
-      address,
-    };
+    console.log(formData);
+    return;
     try {
-      await axios.post(
+      const response = await axios.post(
         "http://localhost:4000/api/v1/appointment/post",
-        formData
+        formData,
+        { withCredentials: true }
       );
-      toast.success("Registration Successfull");
-    } catch (err) {
-      toast.error(err.response.data);
+      toast.success("Booked appointment");
+    } catch (error) {
+      console.error("Failed to book appointment:", error);
+      const msg = error.data?.message || error.message;
+      toast.error(msg);
+    }
+  };
+
+  const isDoctorAvailable = (date) => {
+    const day = date.getDay();
+    const availability = {
+      1: selectedDoctor?.monday || 0,
+      2: selectedDoctor?.tuesday || 0,
+      3: selectedDoctor?.wednesday || 0,
+      4: selectedDoctor?.thursday || 0,
+      5: selectedDoctor?.friday || 0,
+      6: selectedDoctor?.saturday || 0,
+      0: selectedDoctor?.sunday || 0,
+    };
+    return availability[day];
+  };
+
+  const getMinTime = (date) => {
+    if (isDoctorAvailable(date)) {
+      const today = new Date();
+      today.setHours(
+        parseInt(selectedDoctor?.startTime?.split(":")[0] || 0),
+        parseInt(selectedDoctor?.startTime.split(":")[1] || 0),
+        0
+      );
+      return today;
+    }
+  };
+
+  const getMaxTime = (date) => {
+    if (isDoctorAvailable(date)) {
+      const today = new Date();
+      today.setHours(
+        parseInt(selectedDoctor?.endTime?.split(":")[0] || 23),
+        parseInt(selectedDoctor?.endTime.split(":")[1] || 59),
+        0
+      );
+      return today;
     }
   };
 
@@ -59,8 +150,9 @@ export default function AppointmentForm() {
           type="text"
           className="cs_form_field"
           placeholder="Sovan"
-          value={firstname}
-          onChange={(e) => setFirstName(e.target.value)}
+          name="firstName"
+          value={formData.firstName}
+          onChange={handleChange}
         />
         <div className="cs_height_42 cs_height_xl_25" />
       </div>
@@ -70,8 +162,9 @@ export default function AppointmentForm() {
           type="text"
           className="cs_form_field"
           placeholder="Roy"
-          value={lastname}
-          onChange={(e) => setLastName(e.target.value)}
+          name="lastName"
+          value={formData.lastName}
+          onChange={handleChange}
         />
         <div className="cs_height_42 cs_height_xl_25" />
       </div>
@@ -79,9 +172,10 @@ export default function AppointmentForm() {
         <label className="cs_input_label cs_heading_color">Phone Number</label>
         <input
           type="number"
+          name="phone"
           className="cs_form_field"
-          value={phone}
-          onChange={(e) => setPhone(e.target.value)}
+          value={formData.phone}
+          onChange={handleChange}
         />
         <div className="cs_height_42 cs_height_xl_25" />
       </div>
@@ -99,15 +193,16 @@ export default function AppointmentForm() {
             isClearable
             placeholderText="dd/mm/yyyy"
           /> */}
-          <Datetime
-            value={selectedDate}
-            onChange={(date) => setSelectedDate(date)}
-            inputProps={{ placeholder: "dd/mm/yyyy hh:mm" }}
-            dateFormat="DD/MM/YYYY"
-            timeFormat="HH:mm"
-            closeOnSelect
-            isClearable
-            placeholderText="dd/mm/yyyy hh:mm"
+          <DatePicker
+            id="appointmentDateTime"
+            selected={formData.appointmentDateTime}
+            onChange={handleDateChange}
+            filterDate={isDoctorAvailable}
+            showTimeSelect
+            minTime={getMinTime(formData.appointmentDateTime)}
+            maxTime={getMaxTime(formData.appointmentDateTime)}
+            dateFormat="MMMM d, yyyy h:mm aa"
+            className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
           />
         </div>
         <div className="cs_height_42 cs_height_xl_25" />
@@ -116,17 +211,15 @@ export default function AppointmentForm() {
         <label className="cs_input_label cs_heading_color">Date of Birth</label>
 
         <div className="cs_with_icon_input">
-          <DatePicker
-            selected={dob}
-            onChange={(date) => setSelectedDateofBirth(date)}
-            dateFormat="dd/MM/yyyy"
-            minDate={new Date()}
-            isClearable
-            placeholderText="dd/mm/yyyy"
+          <input
+            type="date"
+            id="dob"
+            name="dob"
+            value={formData.dob}
+            onChange={handleChange}
+            className="cs_form_field"
+            required
           />
-          <i>
-            <Icon icon="fa6-solid:calendar-days" />
-          </i>
         </div>
         <div className="cs_height_42 cs_height_xl_25" />
       </div>
@@ -136,8 +229,9 @@ export default function AppointmentForm() {
           type="email"
           className="cs_form_field"
           placeholder="sovanroy@gmail.com"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
         />
         <div className="cs_height_42 cs_height_xl_25" />
       </div>
@@ -147,8 +241,8 @@ export default function AppointmentForm() {
           className="cs_form_field"
           name="department"
           id="department"
-          onChange={(e) => setSelectedDepartment(e.target.value)}
-          value={selectedDepartment}
+          onChange={handleChange}
+          value={formData.department}
         >
           {departments.map((department) => (
             <option value={department}>
@@ -160,23 +254,30 @@ export default function AppointmentForm() {
 
       <div className="col-lg-6">
         <label className="cs_input_label cs_heading_color">Doctor Name</label>
-        <input
-          type="text"
+        <select
           className="cs_form_field"
-          placeholder="Dr Rupal Paul"
-          value={doctor_name}
-          onChange={(e) => setDoctor(e.target.value)}
-        />
+          id="doctor_name"
+          name="doctor_name"
+          value={formData.doctor_name}
+          onChange={handleChange}
+        >
+          {doctors.map((doctor) => (
+            <option key={doctor._id} value={doctor.name}>
+              {doctor.name}
+            </option>
+          ))}
+        </select>
         <div className="cs_height_42 cs_height_xl_25" />
       </div>
       <div className="col-lg-12">
         <label className="cs_input_label cs_heading_color">Address</label>
         <input
           type="text"
+          name="address"
           className="cs_form_field"
           placeholder="Your address"
-          value={address}
-          onChange={(e) => setAddress(e.target.value)}
+          value={formData.address}
+          onChange={handleChange}
         />
         <div className="cs_height_42 cs_height_xl_25" />
       </div>
@@ -191,8 +292,8 @@ export default function AppointmentForm() {
               id="maleGender"
               defaultValue="routineCheckup"
               value="Male"
-              checked={gender === "Male"}
-              onChange={handleGenderChange}
+              checked={formData.gender === "Male"}
+              onChange={handleChange}
             />
             <label className="cs_radio_label" htmlFor="routineCheckup">
               Male
@@ -207,8 +308,8 @@ export default function AppointmentForm() {
               defaultValue="newPatientVisit"
               defaultChecked=""
               value="Female"
-              checked={gender === "Female"}
-              onChange={handleGenderChange}
+              checked={formData.gender === "Female"}
+              onChange={handleChange}
             />
             <label className="cs_radio_label" htmlFor="newPatientVisit">
               Female
