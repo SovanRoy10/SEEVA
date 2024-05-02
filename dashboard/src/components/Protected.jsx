90f1f53856665dcc58c1f68f778a5fdceb8801e3
Sovan Roy <roysovan00@gmail.com>
2024-05-03 01:11:15 +0530

    use redux in Protected.jsx

--- dashboard/src/components/Protected.jsx
+++ dashboard/src/components/Protected.jsx
@@ -2,21 +2,23 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import Cookies from "js-cookie";
 import Loader from './loader/Loader';
+import { useSelector } from "react-redux";
 
 export default function Protected({ children }) {
+  const user = useSelector((state) => state.user.user);
   const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(true); 
-  const adminToken = Cookies.get("AdminToken");
+  // const adminToken = Cookies.get("AdminToken");
 
   useEffect(() => {
-    if (!adminToken) {
+    if (!user) {
       navigate("/login");
     } else {
       setIsLoading(false); // Set loading to false if token exists
     }
-  }, [adminToken, navigate]);
+  }, [user, navigate]);
 
-  if (!adminToken || isLoading) {
+  if (!user || isLoading) {
     return <Loader/>; // Show loading or any other fallback UI
   }
 
