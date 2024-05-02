74830935bb2b236c53b4ac6f9102b97e93f0ac73
Sovan Roy <roysovan00@gmail.com>
2024-05-03 01:23:13 +0530

    testing

--- server/controller/user.js
+++ server/controller/user.js
@@ -443,12 +443,9 @@ export const login = catchAsyncErros(async (req, res, next) => {
         expires: new Date(
           Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
         ),
-        // httpOnly: true,
-        // secure: true, // Ensure cookies are sent over HTTPS
-        // sameSite: "None", // Essential for cross-site/cross-origin requests
-        domain: "seeva-server.vercel.app",
-        path: "/",
         httpOnly: true,
+        secure: true, // Ensure cookies are sent over HTTPS
+        sameSite: "None", // Essential for cross-site/cross-origin requests
       })
       .json({
         success: true,
